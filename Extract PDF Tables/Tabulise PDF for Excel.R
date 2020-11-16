71fdfabcca9315f008a0a7c057fa0f7067cd1e4f
Paul I Ighofose <paul@ipi-international.co.uk>
2020-11-16 00:33:05 +0000

    Extract PDF Tables using Power Query or R Scripts
    Excel Macro driven file that uses two methods of extracting Tables from a PDF File to populate tables in the Excel file. R Script has been written specifically for the PDF example available here.

---
+++ Extract PDF Tables/Tabulise PDF for Excel.R
@@ -0,0 +1,16 @@
+library(tabulizer)
+fPath <- xlPath
+location <- file.path(fPath, "..", "pdf", "Abbreviation-List.pdf")
+out <- extract_tables(location)
+final <- do.call(rbind, out)
+states <- rbind(as.matrix(final[2:31, 1:2], ncol=2), as.matrix(final[2:31, 3:4], ncol =2))
+states <- states[states[,2] > "",]
+colnames(states) <- as.data.frame(out)[1,1:2]
+write.csv(states, file.path(fPath, "..", "csv", "States.csv"), row.names = FALSE)
+directional <- as.matrix(final[34:41, 1:2], ncol=2)
+colnames(directional) <- lapply(as.data.frame(out)[33,1:2], function(x) {gsub("\r", "\\\n", x)})
+write.csv(directional, file.path(fPath, "..", "csv", "Directional.csv"), row.names = FALSE)
+address <- as.matrix(final[34:41, 3:4], ncol =2)
+address <- address[address[,2] > "",]
+colnames(address) <- lapply(as.data.frame(out)[33,3:4], function(x) {gsub("\r", "\\\n", x)})
+write.csv(address, file.path(fPath, "..", "csv", "Address.csv"), row.names = FALSE)
